fef0572e2f1eaa33e653b20ccf76530974958c83
vvenedik <vvenedik@NB-17-1088>
2016-05-11 15:53:37 -0700

    add for CLI/C++

---
+++ IBJts/order_book/interactive.cpp
@@ -0,0 +1,32 @@
+
+#include "interactive.hpp"
+#include "memory_types.hpp"
+#include "entity_cache.hpp"
+#include "order_entity.hpp"
+
+using mpclmi::ipc::Shared;
+using ipc::data::order_entity;
+
+namespace interactive {
+    
+    OrderBookCache::collection_t INTERACTIVE_DLL_EXPORTS
+    OrderBookCache::GetEntries() {
+        std::vector<std::shared_ptr < OrderContract>> entries;
+        using Cache = datacache::entity_cache<Shared, ipc::data::order_container>;
+        Cache ipc_order_book(cache_name_);
+        ipc_order_book.retrieve(entries);
+        return entries;
+    }
+
+    OrderBookCache::collection_t INTERACTIVE_DLL_EXPORTS
+    OrderBookCache::GetEntriesByAccount(const std::string &account) {
+        using Cache = datacache::entity_cache<Shared, ipc::data::order_container>;
+        using Tag = typename ipc::data::order_entity<Cache::char_allocator>::account_ticker_tag;
+        std::vector<std::shared_ptr < OrderContract>> entries;
+        Cache ipc_order_book(cache_name_);
+        auto acct_key = ipc_order_book.create_ipc_key(account);
+        ipc_order_book.retrieve<Tag>(entries, acct_key);
+        return entries;
+    }
+	
+}
