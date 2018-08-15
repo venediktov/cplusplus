e4a1561ffe4d8d5e3f4963ff3447e828664b2048
Vladimir Venediktov <venediktov@users.noreply.github.com>
2018-08-14 18:00:08 -0700

    Update eth_eos.js

--- eth_eos.js
+++ eth_eos.js
@@ -15,18 +15,18 @@ const message = 'EOS can handle ETH signatures';
 const messageHash = EthCrypto.hash.keccak256(message);
 console.log("messageHash:");
 console.log(messageHash);
-const signature = EthCrypto.sign(
+const eth_signature = EthCrypto.sign(
     private_key_hex_str, // privateKey
     messageHash // hash of message
 );
-console.log('signature:');
-console.log(signature);
-const signer = EthCrypto.recoverPublicKey(
-    signature, //'0x388862e1a8e03c0d5012ca201c65c0eaecbe13f7418444e8788cd2f358bf254214d413e7e033d08629cd3a50ed413e947d289e2bedf34a133048b39fa509d7cc1c', // signature
+console.log('eth_signature:');
+console.log(eth_signature);
+const eth_signer = EthCrypto.recoverPublicKey(
+    eth_signature, //'0x388862e1a8e03c0d5012ca201c65c0eaecbe13f7418444e8788cd2f358bf254214d413e7e033d08629cd3a50ed413e947d289e2bedf34a133048b39fa509d7cc1c', // signature
     messageHash //'0xf994cb4eec41aac621f0618bdd498c0c529dcf7c86e31e66d953f4b6d4299d15' // message hash
 );
-console.log('signer:');
-console.log(signer);
+console.log("PUBLIC KEY RECOVERY ( ETH Library) :");
+console.log(eth_signer.slice(0,64) + " + " + eth_signer.slice(64));
 console.log("######################################   EOS");
 
 // ecc.randomKey().then(privateKey => {
@@ -47,7 +47,7 @@ private_key = Buffer.from(private_key_hex_str.slice(2),'hex');
 private_key= Buffer.concat([new Buffer([0x80]), private_key]);
 eos_private_key_wif = keyUtils.checkEncode(private_key, 'sha256x2');
 
-console.log("eos_private_key_base58:");
+console.log("eos_private_key_wif:");
 console.log(eos_private_key_wif);
 console.log(ecc.isValidPrivate(eos_private_key_wif) === true);
 
@@ -56,7 +56,7 @@ console.log(message);
 console.log("messageHash:")
 console.log(messageHash)
 
-const eos_signature = ecc.sign(message, eos_private_key_wif, 'hex');
+const eos_signature = ecc.sign(message, eos_private_key_wif);
 const eos_signed_hash = ecc.signHash(messageHash.slice(2), eos_private_key_wif, 'hex');
 
 console.log("eos_signature for plain message:");
@@ -64,13 +64,13 @@ console.log(eos_signature);
 console.log("eos_signature for hash message:");
 console.log(eos_signed_hash);
 
-console.log("PUBLIC KEY RECOVERY ( EOS ) :");
-const ms = Buffer.from( messageHash, 'hex');
-const e = ecc.recover(eos_signature, ms);
-console.log(e);
-
-//Public key In hex
-console.log(keyUtils.checkDecode(e.slice(3)).toString('hex')); 
+console.log("PUBLIC KEY RECOVERY ( EOS Library ) :");
+const ms = Buffer.from( messageHash.slice(2), 'hex');
+// const e = ecc.recover(eos_signature, ms);
+const e = ecc.recoverHash(eos_signed_hash, ms);
+const prefix = keyUtils.checkDecode(e.slice(3)).toString('hex').slice(0,2);
+const key = keyUtils.checkDecode(e.slice(3)).toString('hex').slice(2);
+console.log(e + " ===> " + prefix + " + " + key );
 
 console.log("Execute following chain command:")
 cleosCmd = "cleos push action sense.snap keycheck '[" + messageHash.slice(2) + " , " +  eos_signed_hash + "]' -p sense.snap";
@@ -81,6 +81,5 @@ eos_signed_hash_hex = keyUtils.checkDecode(eos_signed_hash.slice(7), 'K1').toStr
 console.log(eos_signed_hash_hex.slice(0,2) + " + " + eos_signed_hash_hex.slice(2));
 
 console.log("ETH SIG IN HEX:")
-eth_signed_hash_hex = signature.slice(2);
+eth_signed_hash_hex = eth_signature.slice(2);
 console.log(eth_signed_hash_hex.slice(0,-2) + " + " + eth_signed_hash_hex.slice(-2));
-
